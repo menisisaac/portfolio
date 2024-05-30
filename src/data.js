71f6919e5fc21743ba8fa536c97bd91f7bd4e141
menisisaac <imenis@albany.edu>
2024-05-30 10:56:28 -0400

    init

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const projects = [
+  {
+    title: "Internet Printing Protocol",
+    subtitle: "C++ Implementation of RFC8010&8011",
+    description:
+      "An implementation of the internet printing protocol designed to enable printing in SerenityOS?",
+    image: "./printer.jpeg",
+    link: "https://github.com/menisisaac/ipp",
+  },
+  {
+    title: "RISCV Processor",
+    subtitle: "Softcore implementation of RISCV spec",
+    description:
+      "A 3 staged pipelined processor with branch predication and precise exceptions. Implemented in verilog and verified with verilator",
+    image: "./pipeline.png",
+    link: "https://github.com/menisisaac/",
+  },
+  {
+    title: "Mobile Spectrum Analyzer",
+    subtitle: "Wifi and Cellular Data Collection",
+    description:
+      "Implemented an Android app which collects spatial and temporal data along with wifi and cellular scans in the background every minute into a SQLite database. Wrote python scripts to automate data analysis.",
+    image: "./spectrum.jpg",
+    link: "https://github.com/menisisaac/",
+  },
+  {
+    title: "Shank Lanuage Interpreter & Compiler",
+    subtitle: "Used Java and Bril intermediate representation",
+    description:
+      "Utilized recursive decent parsing to create an AST. Executed that AST in java for the in class part. Compiled down to x64 using Bril intermediate language after the semester was over",
+    image: "./compiler.png",
+    link: "https://github.com/menisisaac/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "C++",
+  "Verilog",
+  "Python",
+  "SQL",
+  "Networking",
+  "Operating Systems",
+  "Digital Design/Computer Architecture",
+  "Databases"
+];
